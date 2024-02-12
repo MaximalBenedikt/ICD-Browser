6756d69892f93b0b5f38f5445a6c009864b977bf
Maximal Benedikt <github@maximalbenedikt.de>
2024-02-12 10:23:12 +0100

    getToken Error

--- backend/getToken.php
+++ backend/getToken.php
@@ -2,18 +2,17 @@
 include "config.php"
 
 // Get OAUTH2 token
-$OAuthTokenRequest = curl_init();
-curl_setopt($OAuthTokenRequest, CURLOPT_URL, $tokenEndpoint);
-curl_setopt($OAuthTokenRequest, CURLOPT_POST, TRUE);
-curl_setopt($OAuthTokenRequest, CURLOPT_POSTFIELDS, array(
+$AuthTokenRequest = curl_init();
+curl_setopt($AuthTokenRequest, CURLOPT_URL, $tokenEndpoint);
+curl_setopt($AuthTokenRequest, CURLOPT_POST, TRUE);
+curl_setopt($AuthTokenRequest, CURLOPT_POSTFIELDS, array(
 			'client_id' => $clientId,
 			'client_secret' => $clientSecret,
 			'scope' => $scope,
 			'grant_type' => $grant_type
 ));
-curl_setopt($OAuthTokenRequest, CURLOPT_RETURNTRANSFER, 1); 
-$result = curl_exec($OAuthTokenRequest);
+curl_setopt($AuthTokenRequest, CURLOPT_RETURNTRANSFER, 1); 
+$result = curl_exec($AuthTokenRequest);
 $json_array = (json_decode($result, true));
 $token = $json_array['access_token'];
-curl_close($OAuthTokenRequest);
-?>
+curl_close($AuthTokenRequest);
