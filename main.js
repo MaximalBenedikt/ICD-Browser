ca784d501d48e1cf01cc158d030d5b33a0418b11
Maximal Benedikt <github@maximalbenedikt.de>
2024-02-12 23:25:59 +0100

    Test

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const BaseUrl="backend/"
+var Content = {};
+
 /** 
  * Represents one Entity
  * @class
@@ -17,7 +19,7 @@ class Entity{
     //Link
     link={} //release -> lang -> string
     
-    constructor (id, release, language, cb=()=>{}, debug_loadAll = true) {
+    constructor (id, release, language, cb=()=>{}, debug_loadAll = false) {
         this.id = id;
         console.log(id)
         let submitData = {};
@@ -39,18 +41,32 @@ class Entity{
 			
        console.log(BaseUrl + "getEntity.php");
         $.get(BaseUrl + "getEntity.php", submitData, (rawData)=>{
-        console.log(rawData)
+            console.log(rawData)
         	let data = JSON.parse(rawData);
             let dataRelease = data.releaseId;
-            let dataLang = data[""];
+            let dataLang = data["title"]["@language"];
             
             console.log(rawData);
 
-            if (typeof this.link[dataRelease] != undefined) {}
-        }).fail((data)=>{
-        	  // alert("Error");
-            console.log(data.getResponseHeader());
-        });
+            if (typeof this.link[dataRelease] != undefined) this.link[dataRelease] = {};
+            if (typeof this.link[dataRelease][dataLang] != undefined) this.link[dataRelease][dataLang] = data["browserUrl"];
+
+            if (typeof this.names[dataRelease] != undefined) this.names[dataRelease] = {};
+            if (typeof this.names[dataRelease][dataLang] != undefined) this.names[dataRelease][dataLang] = data["title"]["@value"];
+
+            data.child.forEach(newChild => {
+                let newChildLink = newChild.split("/");
+                this.child.push(newChildLink[newChildLink.length - 1]);
+                if (debug_loadAll) {
+                    await Sleep(1000);
+                    new Entity(newChildLink[newChildLink.length - 1], undefined, undefined, ()=>{}, debug_loadAll);
+                }
+            });
+
+
+            Content[this.id] = this;
+            console.log(this);
+        })
     }
 }
 
