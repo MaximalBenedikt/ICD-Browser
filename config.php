601ef664d2e67de375a53d21d92648a5f639e896
Maximal Benedikt <github@maximalbenedikt.de>
2024-02-13 21:00:06 +0100

    Large Update

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$tokenEndpoint = "https://icdaccessmanagement.who.int/connect/token";
+$clientId = "2340f0e3-7e65-4317-a699-af2f55fe7129_8409f5cb-252a-4926-af41-1b0b8622919d"; //If you enter your Clientid
+$clientSecret = "HlcvLZn7vLQPrqjS0b20h44L83PCXgPo3jabMcRinB0="; //and Secret, in your sourcecode: Well Played :P
+$scope = "icdapi_access";
+$grant_type = "client_credentials";
+?>
---
+++ home.php
@@ -0,0 +1,1 @@
+<?php echo "MOM I'M HOME!" ?>
